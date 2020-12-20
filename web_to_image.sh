430487f8864b944d170281424f15818954d38fb7
drdr xp <drdr.xp@gmail.com>
2020-12-20 16:40:32 +0800

    A  web_to_image.sh

---
+++ web_to_image.sh
@@ -0,0 +1,6 @@
+'/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'  \
+    --headless  \
+    --screenshot  \
+    --window-size=400,4000  \
+    --default-background-color=0  \
+    qqq.html
