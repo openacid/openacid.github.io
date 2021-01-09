adc333cb3c0f01273a6a7b5a9a23e3951e3dee24
drdr xp <drdr.xp@gmail.com>
2021-01-09 18:15:38 +0800

    add convert.sh to convert markdown to platform compatible format. Currently it support zhihu, weibo and wechat

---
+++ convert.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+fn="$(ls _posts/*$1* | head -n1)"
+
+if [ "$fn." = "." ]; then
+    echo "Usage: $0 <fn_pattern>"
+    exit 1
+fi
+
+
+# md2zhihu -r git@gitee.com:drdrxp/bed.git@openacid $fn -p wechat --keep-meta --code-width 600 -o $fn-wechat.md
+md2zhihu -r git@gitee.com:drdrxp/bed.git@openacid $fn -p weibo --keep-meta --code-width 600 -o $fn-weibo.md
