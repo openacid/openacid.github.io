e63b1919f1cf7c45aacbdab9cc24c5b3bc728580
drmingdrmer <drdr.xp@gmail.com>
2021-06-19 22:53:57 +0800

    by md2zhihu by drdr.xp@gmail.com

---
+++ t.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+
+find _src -name "*.md" \
+    | while read fn; do
+    name=${fn##*/}
+
+    platform=wechat
+    md2zhihu \
+        --platform         $platform \
+        --code-width       600 \
+        --refs             _data/refs.yml \
+        --keep-meta \
+        --output-dir       . \
+        --asset-output-dir ./md2/$platform \
+        --md-output        ./md2/$platform/ \
+        --repo             .@try-md2$platform \
+        $fn
+
+done
+
